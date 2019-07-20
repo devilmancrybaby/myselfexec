68b09b62de9379a63db92b9cc6679bea8c7b9a37
devilmancrybaby <a9ydeai@163.com>
2019-07-20 10:18:41 +0800

    7-20

--- chapter-5/excerice5-11.c
+++ chapter-5/excerice5-11.c
@@ -1,14 +1,25 @@
+//编写一段程序，输入6名学生2门课程(语文、数学)的分数，
+//显示各门课程的总分和平均分，以及各个学生的总分和平均分。
 #include <stdio.h>
-int main(void){
-	int stu[6][2];
-	int i, j;
+int main(void) {
+    int stu[6][2];
+    int i, j;
+    int sum1 = 0;
+    int sum2 = 0;
 
-	printf("输入学生分数: ");
+    printf("输入学生分数: ");
 
-	for(i=0;i<6;i++)
-		for(j=0;j<2;j++){
-			printf("No.%d学生两门成绩: ",i);
-			scanf("%d",&stu[i][j]);}
+    for (i = 0; i < 6; i++)
+        for (j = 0; j < 2; j++) {
+            printf("No.%d学生两门成绩: ", i + 1);
+            scanf("%d", &stu[i][j]);
+        }
 
-			return 0;
+    for (i = 0; i < 6; i++) {
+        sum1+= stu[i][0];}
+        printf("%d\n",sum1 );
+    
+
+
+    return 0;
 }
--- chapter-6/excerice6-12.c
+++ chapter-6/excerice6-12.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
-int mat_mul(const int a[4][3], const int b[3][4], int c[3][3]) {
+void mat_mul(const int a[4][3], const int b[3][4], int c[3][3]) {
     int i, j, k;
 
-    for (i = 0; i < 4; i++)
-        for (j = 0; j < 3; j++) {
 
-            printf("%d ", a[i][j]);
-            printf("\n");
-        }
-
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 4; j++) {
-            printf("%d ", b[i][j]);
-            printf("\n");
-        }
 
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
@@ -23,28 +12,45 @@ int mat_mul(const int a[4][3], const int b[3][4], int c[3][3]) {
         }
     }
 
-    return c[i][j];
+
 
 }
 
 int main(void) {
-    int a[4][3];
-    int b[3][4];
-    int c[3][3];
-    int i, j;
+    int a[4][3]={0};
+    int b[3][4]={0};
+    int c[3][3]={0};
+    int i, j,k;
+
+
+    puts("a组值");
 
     for (i = 0; i < 4; i++)
         for (j = 0; j < 3; j++) {
+            printf("第%d行，第%d列的值:\n", i + 1, j + 1);
             scanf("%d", &a[i][j]);
         }
 
-    for (i = 0; i < 4; i++)
+    puts("b组值");
+
+    for (i = 0; i < 3; i++)
         for (j = 0; j < 4; j++) {
+            printf("第%d行，第%d列的值:\n", i + 1, j + 1);
+
             scanf("%d", &b[i][j]);
 
         }
 
-    printf("%d", mat_mul(a, b, c));
+    mat_mul(a, b, c);
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf(" %d ", c[i][j]);
+
+        }
+
+        printf("\n");
+    }
 
     return 0;
 }
